df64e2c34817946576b6fedb5b4d99b5f3d83d81
Aerish <aryalaerish369@gmail.com>
2025-11-06 14:43:16 +0545

    Fix client folder to be normal folder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/hello/")
+      .then((res) => setMessage(res.data.message));
+  }, []);
+
+  return <h1>{message}</h1>;
+}
+
+export default App;
