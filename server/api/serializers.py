e5020cbc3a51997c046cdde60f3f1b0591c86d35
Aerish <aryalaerish369@gmail.com>
2025-11-18 16:31:26 +0545

    profile api created with update feature'

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
     class Meta: 
         model = Profile
         fields = ['id', 'user_id', 'phone', 'birth_date', 'location', 'skills', 'experience', 'education']
--- server/api/views.py
+++ server/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyM
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -15,5 +16,14 @@ class ProfileViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, Gen
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     
-    def me(self, request):
-        pass
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request): #api/profile/me
+        (profile, created) = Profile.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = ProfileSerializer(profile, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
