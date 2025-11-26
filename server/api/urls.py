0396b998097eb73ba9d663050e084e7555924b1e
Aerish <aryalaerish369@gmail.com>
2025-11-26 14:05:02 +0545

    Created JobViewSet and used Router

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,12 +4,20 @@ from . import views
 
 
 router = DefaultRouter()
-router.register(r'profile', views.ProfileViewSet, basename='profile') #http://127.0.0.1:8000/api/profile/me/
+router.register(r'profile', views.ProfileViewSet, basename='profile') #http://127.0.0.1:8000/api/profile/me/\
+router.register('jobs', views.JobViewSet)
 
 urlpatterns = [
     path('hello/', views.hello),
-    path('jobs/', views.JobListAPIView.as_view(), name='job-list'), # /api/jobs/
-    path('jobs/<int:pk>/', views.JobDetailAPIView.as_view(), name='job-detail'), # api/jobs/pk
-    path('jobs/create/', views.JobCreateAPIView.as_view(), name='job-create'), # POST /jobs/create/
+    # /api/jobs/
+    # api/jobs/pk
+    # POST /jobs/create/
+    # DELETE /api/jobs/12/delete/
     path('jobs/<int:job_id>/apply/', views.ApplyJobAPIView.as_view(), name='apply-job'), #POST /jobs/<job_id>/apply/
 ] + router.urls
+
+
+
+#! Make Job View into one by combining other job views.
+#! Use Router for that. 
+#! 
